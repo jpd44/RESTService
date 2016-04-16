14f6b1373d6c762031e6ac5e6937e7741810a500
jpd44 <jpdixon@gmail.com>
2016-04-16 14:32:31 -0400

    first commit

---
+++ grails-app/conf/spring/resources.groovy
@@ -0,0 +1,20 @@
+// Place your Spring DSL code here
+import grails.rest.render.xml.*
+import grails.rest.render.json.JsonRenderer
+import restservice.City
+
+beans = {
+    cityXmlRenderer(XmlRenderer, City) {
+        excludes = ['class','dateCreated']
+    }
+    cityJsonRenderer(JsonRenderer, City) {
+        excludes=['class','dateCreated']
+    }
+    CityXmlCollectionRenderer(CityXmlCollectionRenderer, City) {
+        excludes = ['class','dateCreated']
+    }
+    CityJsonCollectionRenderer(CityJsonCollectionRenderer, City) {
+        excludes = ['class','dateCreated']
+    }
+
+}
